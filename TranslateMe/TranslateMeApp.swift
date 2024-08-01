39b810976519dac716f328d1986160ee4ef2a16c
Raymond Chen <ahrchen@gmail.com>
2024-07-31 21:47:59 -0700

    Initial Commit

---
+++ TranslateMe/TranslateMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateMeApp.swift
+//  TranslateMe
+//
+//  Created by Raymond Chen on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct TranslateMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
